1674e6ac55cb387f62e8387f9d802e15f381cdac
reynal240212 <124523746+reynal240212@users.noreply.github.com>
2025-02-19 18:28:31 +0000

    ajuste y creacion del backend para el login

---
+++ mi-backend/routes/web.php
@@ -0,0 +1,8 @@
+<?php
+use App\Http\Controllers\Auth\SocialController;
+
+// Ruta para redireccionar al proveedor
+Route::get('/auth/google', [SocialController::class, 'redirectToGoogle'])->name('google.login');
+
+// Ruta para manejar la respuesta del proveedor (callback)
+Route::get('/auth/google/callback', [SocialController::class, 'handleGoogleCallback'])->name('google.callback');
