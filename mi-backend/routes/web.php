ecdd17b1e4f014030d8c85f792b43f258428e526
reynal240212 <124523746+reynal240212@users.noreply.github.com>
2025-02-19 18:54:06 +0000

    .

--- mi-backend/routes/web.php
+++ mi-backend/routes/web.php
@@ -2,7 +2,7 @@
 use App\Http\Controllers\Auth\SocialController;
 
 // Ruta para redireccionar al proveedor
-Route::get('/auth/google', [SocialController::class, 'redirectToGoogle'])->name('google.login');
+Route::get('https://https://diba-pi.vercel.app//auth/google/callback', [SocialController::class, 'redirectToGoogle'])->name('google.login');
 
 // Ruta para manejar la respuesta del proveedor (callback)
 Route::get('/auth/google/callback', [SocialController::class, 'handleGoogleCallback'])->name('google.callback');
