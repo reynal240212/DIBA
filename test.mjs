03a20aa64b0a3e9d014e13c53113af8e8392d210
reynal240212 <124523746+reynal240212@users.noreply.github.com>
2025-02-25 22:13:21 +0000

    .

--- test.mjs
+++ test.mjs
@@ -1,13 +1,1 @@
-import { supabase } from './supabaseClient.js';
-
-async function testConnection() {
-  let { data, error } = await supabase.from('Categoria_2014_2015_2016').select('*');
-  
-  if (error) {
-    console.error('Error al obtener datos:', error);
-  } else {
-    console.log('Datos obtenidos:', data);
-  }
-}
-
-testConnection();
+node test.mjs
