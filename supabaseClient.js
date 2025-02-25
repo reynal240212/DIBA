60020377e38c07efa556c4350da8bd48c2b8cb6d
reynal240212 <124523746+reynal240212@users.noreply.github.com>
2025-02-25 21:47:54 +0000

    .

---
+++ supabaseClient.js
@@ -0,0 +1,7 @@
+import { createClient } from '@supabase/supabase-js';
+
+// Reemplaza con tus credenciales de Supabase
+const supabaseUrl = 'https://nwxdshisfyenkylgqxgz.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im53eGRzaGlzZnllbmt5bGdxeGd6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzkwNTQzMDgsImV4cCI6MjA1NDYzMDMwOH0.QoIiNo5pFA1_MVfE2ugLgyz4HeET-WhA0C_sNOkWv9g';
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
